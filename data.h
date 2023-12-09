af0f2ade507a1b75b79147041259c7d97a8547da
Victor Jansen <victorfjansen@gmail.com>
2023-12-09 18:32:30 -0300

    feat: adiciona análise léxica (reconhecimento de tokens)

---
+++ data.h
@@ -0,0 +1,7 @@
+#ifndef EXTERN_
+ #define EXTERN_ extern
+#endif
+
+EXTERN_ int Line;
+EXTERN_ int	Putback;
+EXTERN_ FILE *Infile;
---
+++ defs.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Structure and enum definitions
+// Copyright (c) 2019 Warren Toomey, GPL3
+
+// Structure and enum definitions
+// Copyright (c) 2019 Warren Toomey, GPL3
+
+// Tokens
+enum {
+    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INT_LITERAL
+};
+
+// Token structure
+struct token {
+    int token;
+    int intValue;
+};
+
